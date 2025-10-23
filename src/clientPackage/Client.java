d8083496388378283c0dbff4f8fe75098060d621
mohamed ali ben slama <mohamedalibenslama0123@gmail.com>
2025-10-23 23:43:40 +0100

    ACT 3-1

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -1,5 +1,33 @@
 package clientPackage;
 
-public class Client {
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 1234;
+        try (Socket socket = new Socket(host, port)) {
+            System.out.println(" Connecte au serveur " + host + ":" + port);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Entrez le premier nombre : ");
+            String operande1 = sc.nextLine();
+            System.out.print("Entrez l'operateur (+, -, *, /) : ");
+            String operateur = sc.nextLine();
+            System.out.print("Entrez le deuxieme nombre : ");
+            String operande2 = sc.nextLine();
+            out.println(operande1);
+            out.println(operateur);
+            out.println(operande2);
+            String reponse = in.readLine();
+            System.out.println(" Reponse du serveur : " + reponse);
+            socket.close();
+            sc.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,5 +1,100 @@
 package serverPackage;
 
-public class Server {
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-}
+public class Server extends Thread{
+    private static AtomicInteger clientCount = new AtomicInteger(0);
+    public static void main(String[] args) {
+        new Server().start();
+
+    }
+    public void run(){
+        ServerSocket socketServer = null;
+        try {
+            socketServer = new ServerSocket(1234);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        while (true) 
+        {
+            int clientNumber = clientCount.incrementAndGet();
+            Socket socket = null;
+            try {
+                socket = socketServer.accept();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("nouveau client connecte : " + socket.getRemoteSocketAddress()+ " (Client n " + clientNumber + ")");
+            new ClientProcess(socket,clientNumber).start();
+        }
+    }
+    public class ClientProcess extends Thread {
+        Socket socket;
+        int clientNumber;
+        public ClientProcess(Socket socket,int clientNumber)
+        {
+            this.socket=socket;
+            this.clientNumber=clientNumber;
+        }
+        public void run()
+        {
+            try{            System.out.println("le client n "+clientNumber+" est connecte");
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+           
+            String operande1 = in.readLine();
+            String operateur = in.readLine();
+            String operande2 = in.readLine();
+
+            System.out.println("Reçu client n "+clientNumber+" : " + operande1 + " " + operateur + " " + operande2);
+
+       
+            double resultat = 0;
+            try {
+                double a = Double.parseDouble(operande1);
+                double b = Double.parseDouble(operande2);
+
+                switch (operateur) {
+                    case "+": resultat = a + b; break;
+                    case "-": resultat = a - b; break;
+                    case "*": resultat = a * b; break;
+                    case "/":
+                        if (b != 0) resultat = a / b;
+                        else {
+                            out.println("Erreur");
+                            socket.close();
+                            return;
+                        }
+                        break;
+                    default:
+                        out.println("Erreur");
+                        socket.close();
+                        return;
+                }
+
+                out.println("Resultat de client n "+clientNumber+"= " + resultat);
+                System.out.println("Resultat envoyé au client n "+clientNumber);
+
+            } catch (NumberFormatException e) {
+                out.println("Erreur");
+            }
+
+            socket.close();
+            System.out.println("Connexion fermée.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    }}
+        
+
+    
+        
+    
+
