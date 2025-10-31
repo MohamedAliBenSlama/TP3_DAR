f19d203ba07811586b1c7b609c83388299eec786
mohamed ali ben slama <mohamedalibenslama0123@gmail.com>
2025-10-31 19:51:16 +0100

    ACT 3-3

--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -2,15 +2,21 @@ package serverPackage;
 
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import operationPackage.Operation;
 
 public class Server extends Thread {
-    private static AtomicInteger clientCount = new AtomicInteger(0);
+
+    private static int clientCount = 0; 
     private static int totalOperations = 0;
 
     public static void main(String[] args) {
         new Server().start();
+    }
+
+    // Méthode synchronisée pour incrémenter le nombre de clients
+    public static synchronized int incrementClientCount() {
+        clientCount++;
+        return clientCount;
     }
 
     public void run() {
@@ -23,12 +29,12 @@ public class Server extends Thread {
         }
 
         while (true) {
-            int clientNumber = clientCount.incrementAndGet();
-            Socket socket = null;
+            int clientNumber = incrementClientCount(); // utilisation de la méthode synchronisée
             try {
-                socket = socketServer.accept();
-                System.out.println("nouveau client connecte : " + socket.getRemoteSocketAddress()+ " (Client n " + clientNumber + ")");
-                new ClientProcess(socket,clientNumber).start();
+                Socket socket = socketServer.accept();
+                System.out.println("Nouveau client connecté : " 
+                    + socket.getRemoteSocketAddress() + " (Client n° " + clientNumber + ")");
+                new ClientProcess(socket, clientNumber).start();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -38,20 +44,20 @@ public class Server extends Thread {
     public class ClientProcess extends Thread {
         Socket socket;
         int clientNumber;
-        public ClientProcess(Socket socket,int clientNumber)
-        {
-            this.socket=socket;
-            this.clientNumber=clientNumber;
+
+        public ClientProcess(Socket socket, int clientNumber) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
         }
-        public void run()
-        {
-            try{
-                System.out.println("le client n "+clientNumber+" est connecte");
+
+        public void run() {
+            try {
                 ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
                 ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 
                 Operation op = (Operation) in.readObject();
-                System.out.println("Reçu client n "+clientNumber+" : " + op.getOperande1() + " " + op.getOperateur() + " " + op.getOperande2());
+                System.out.println("Reçu du client n° " + clientNumber + " : "
+                        + op.getOperande1() + " " + op.getOperateur() + " " + op.getOperande2());
 
                 double resultat = 0;
                 boolean erreur = false;
@@ -64,24 +70,22 @@ public class Server extends Thread {
                         if (op.getOperande2() != 0) resultat = op.getOperande1() / op.getOperande2();
                         else erreur = true;
                         break;
-                    default:
-                        erreur = true;
-                        break;
+                    default: erreur = true; break;
                 }
 
                 synchronized (Server.class) {
                     totalOperations++;
-                    System.out.println("Nombre total d'opérations : " + totalOperations);
+                    System.out.println("Nombre total d'opérations effectuées : " + totalOperations);
                 }
 
                 if (erreur)
-                    out.writeObject("Erreur");
+                    out.writeObject("Erreur : division par zéro");
                 else
-                    out.writeObject("Resultat de client n "+clientNumber+"= " + resultat);
+                    out.writeObject("Résultat pour client n° " + clientNumber + " = " + resultat);
 
-                System.out.println("Resultat envoyé au client n "+clientNumber);
                 socket.close();
-                System.out.println("Connexion fermée.");
+                System.out.println("Connexion client n° " + clientNumber + " fermée.");
+
             } catch (IOException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
